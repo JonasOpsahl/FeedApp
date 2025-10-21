1f20de5033bd2a32321da7321991b0963e6d286d
Jonas Opsahl <jop011@uib.no>
2025-10-21 14:59:12 +0200

    fixed names

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -4,9 +4,9 @@ plugins {
 	id("io.spring.dependency-management") version "1.1.7"
 }
 
-group = "com.exp2"
+group = "com.gruppe2"
 version = "0.0.1-SNAPSHOT"
-description = "Simple REST API"
+description = "FeedApp backend"
 
 java {
 	toolchain {
