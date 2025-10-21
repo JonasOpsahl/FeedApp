dea87f249756ed1f32b6c6093980fc6d6a21b4cc
Jonas Opsahl <jop011@uib.no>
2025-10-21 14:05:52 +0200

    trial2

---
+++ backend/build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.5.5"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.exp2"
+version = "0.0.1-SNAPSHOT"
+description = "Simple REST API"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-api:2.6.0")
+
+	implementation("org.hibernate.orm:hibernate-core:7.1.1.Final")
+    implementation("jakarta.persistence:jakarta.persistence-api:3.2.0")
+    implementation("com.h2database:h2:2.3.232")
+
+	implementation("redis.clients:jedis:6.2.0")
+	implementation("org.springframework.kafka:spring-kafka")
+	implementation("org.apache.kafka:kafka-clients:3.7.0")
+
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ backend/src/main/java/com/gruppe2/backend/PollApp.java
@@ -0,0 +1,13 @@
+package com.gruppe2.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollApp.class, args);
+	}
+
+}
