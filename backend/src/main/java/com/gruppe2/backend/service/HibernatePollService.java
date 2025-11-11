7b814df64a073178c3151606d43ff764fc86831d
Jonas Opsahl <jop011@uib.no>
2025-11-11 10:02:34 +0100

    typo

--- backend/src/main/java/com/gruppe2/backend/service/HibernatePollService.java
+++ backend/src/main/java/com/gruppe2/backend/service/HibernatePollService.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.security.access.AccessDeniedException; // Import this
+import org.springframework.security.access.AccessDeniedException;
 
 import java.time.Duration;
 import java.time.Instant;
